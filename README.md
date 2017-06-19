8e4929fb241f7004b6561111855814bc9a246c93
Gabriele Tomberli <gabriele.tomberli@gmail.com>
2017-06-19 21:23:31 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,4 @@
+# What is it?
+A simple library to interact with the Telegram Bot API @ https://core.telegram.org/bots/api
+
+
---
+++ src/TelegramBot.js
@@ -0,0 +1,59 @@
+
+const API_URL = 'https://api.telegram.org';
+
+let rp = require( 'request-promise' )
+	, getResult = x => x.result
+	;
+
+const tokenSymbol = Symbol( 'token' );
+
+class TelegramBot {
+
+	constructor( token ) {
+		this[ tokenSymbol ] = token;
+	}
+
+	getMe() {
+		return rp( `${API_URL}/bot${this[tokenSymbol]}/getMe`, {
+			json: true
+		} ).then( getResult );
+	}
+
+	getUpdates( offset, limit ) {
+		return rp( `${API_URL}/bot${this[tokenSymbol]}/getUpdates`, {
+			json: true,
+			body: {
+				offset: offset,
+				limit: limit
+			}
+		} ).then( getResult );
+	}
+
+	sendMessage( chatId, text, options ) {
+		return rp( `${API_URL}/bot${this[tokenSymbol]}/sendMessage`, {
+			json: true,
+			body: Object.assign( {}, options, {
+				chat_id: chatId,
+				text: text
+			} )
+		} ).then( getResult );
+	}
+
+	getFileInfo( fileId ) {
+		return rp( `${API_URL}/bot${this[tokenSymbol]}/getFile`, {
+			json: true,
+			body: {
+				file_id: fileId
+			}
+		} ).then( getResult );
+	}
+
+	downloadFile( filePath ) {
+		return rp( `${API_URL}/file/bot${this[tokenSymbol]}/${filePath}`, {
+			encoding: null
+		} );
+	}
+
+}
+
+module.exports = TelegramBot;
